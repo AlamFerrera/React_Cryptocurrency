8d62c30558a9fedef8100b0fd42c3eac69afc090
AlamFerrera <alamferrera@gmail.com>
2021-12-24 16:00:04 -0400

    Layout Done
    Done

--- cryptocurrency/src/App.js
+++ cryptocurrency/src/App.js
@@ -12,27 +12,38 @@ const App = () => {
                 <Navbar />
             </div>
             <div className="main">
-            <Layout>
-                <div className="routes">
-                <Switch>
-                    <Route exact path="/">
-                        <Homepage />
-                    </Route>
-                    <Route exact path="/exchanges">
-                        <Exchanges />
-                    </Route>
-                    <Route exact path="/cryptocurrencies">
-                        <Cryptocurrencies />
-                    </Route>
-                    <Route exact path="/crypto/:coinId">
-                        <CryptoDetails />
-                    </Route>
-                    <Route exact path="/news">
-                        <News />
-                    </Route>
-                </Switch>
+                <Layout>
+                    <div className="routes">
+                    <Switch>
+                        <Route exact path="/">
+                            <Homepage />
+                        </Route>
+                        <Route exact path="/exchanges">
+                            <Exchanges />
+                        </Route>
+                        <Route exact path="/cryptocurrencies">
+                            <Cryptocurrencies />
+                        </Route>
+                        <Route exact path="/crypto/:coinId">
+                            <CryptoDetails />
+                        </Route>
+                        <Route exact path="/news">
+                            <News />
+                        </Route>
+                    </Switch>
+                    </div>
+                </Layout>
+                <div className='footer' level={5}>
+                    <Typography.Title style={{color: 'white', textAlign: 'center'}}>
+                        Cryptoverse <br />
+                        All Rights Reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
                 </div>
-            </Layout>
             </div>
         </div>
     )
