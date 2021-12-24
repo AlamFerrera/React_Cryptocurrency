ff49a7126354a47be804db71161140193b9b021f
AlamFerrera <alamferrera@gmail.com>
2021-12-24 15:49:56 -0400

    router dom V. updated
    updated to router dom version from 6 to 5.2.0

---
+++ cryptocurrency/src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoDetails = (val) => {
+    return (
+        <div>
+            <h1>CryptoDetails</h1>
+        </div>
+    )
+}
+
+export default CryptoDetails
