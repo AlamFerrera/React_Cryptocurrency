4788395bcb6060080046bf956d1f88c9cb07a72d
AlamFerrera <alamferrera@gmail.com>
2021-12-25 15:11:18 -0400

    Cryptocurrency done
    done

--- cryptocurrency/src/components/Homepage.jsx
+++ cryptocurrency/src/components/Homepage.jsx
@@ -9,7 +9,7 @@ const {Title} = Typography;
 
 const Homepage = () => {
 
-    const {data, isFetching} = useGetCryptosQuery();
+    const {data, isFetching} = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
 
     if(isFetching) return "Loading...";
@@ -47,14 +47,14 @@ const Homepage = () => {
                 <Link to="/cryptocurrencies">Show More</Link>
             </Title>
         </div>
-        <Cryptocurrencies/>
+        <Cryptocurrencies simplified />
         <div className='home-heading-container'>
             <Title level={2} className='home-title'>Latest Crypto News</Title>
             <Title level={3} className='show-more'>
                 <Link to="/news">Show More</Link>
             </Title>
         </div>
-        <News/>
+        <News simplified/>
         </>
     )
 }
